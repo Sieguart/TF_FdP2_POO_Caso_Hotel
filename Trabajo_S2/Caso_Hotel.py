5ee086647d426e8d502b54a558258a718049d202
Sieguart <sieguart1493@gmail.com>
2026-09-21 17:32:11 -0500

    Se agrego Class Empleado

--- Trabajo_S2/Caso_Hotel.py
+++ Trabajo_S2/Caso_Hotel.py
@@ -1,7 +1,22 @@
 #Inicio
 
 Class Empleado:
-    def __init__(self, nombre, apellido, puesto):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.puesto = puesto
+    def __init__(self, dni: str, nombre: str, sueldo_base: float):
+        self._dni = dni
+        self._nombre = nombre
+        self._sueldo_base = sueldo_base
+
+    @property
+    def dni(self):
+        return self._dni
+
+    @property
+    def sueldo_base(self):
+        return self._sueldo_base
+
+#Reglas
+def calcular_afp(self) -> float:
+        return self._sueldo_base * 0.10  # 10% AFP
+
+def calcular_essalud(self) -> float:
+        return self._sueldo_base * 0.08  # 8% EsSalud
