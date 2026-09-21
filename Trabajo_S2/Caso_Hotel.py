f89dd326fc73b387360e1f40e679df663fe1dcfd
Sieguart <sieguart1493@gmail.com>
2026-09-21 17:45:20 -0500

    Se agrego Class Planilla

--- Trabajo_S2/Caso_Hotel.py
+++ Trabajo_S2/Caso_Hotel.py
@@ -1,5 +1,5 @@
 #Inicio
-
+#1
 Class Empleado:
     def __init__(self, dni: str, nombre: str, sueldo_base: float):
         self._dni = dni
@@ -20,3 +20,23 @@ def calcular_afp(self) -> float:
 
 def calcular_essalud(self) -> float:
         return self._sueldo_base * 0.08  # 8% EsSalud
+
+#2
+class EmpleadoPlanilla(Empleado):
+    def __init__(self, dni: str, nombre: str, sueldo_base: float, asignacion_familiar: float = 102.50):
+        super().__init__(dni, nombre, sueldo_base)
+        self._asignacion_familiar = asignacion_familiar
+
+    def calcular_sueldo_neto(self) -> float:
+        descuentos = self.calcular_afp()
+        return self._sueldo_base + self._asignacion_familiar - descuentos
+
+
+class EmpleadoPorHoras(Empleado):
+    def __init__(self, dni: str, nombre: str, horas_trabajadas: int, tarifa_hora: float):
+        super().__init__(dni, nombre, horas_trabajadas * tarifa_hora)
+        self.horas_trabajadas = horas_trabajadas
+        self.tarifa_hora = tarifa_hora
+
+    def calcular_sueldo_neto(self) -> float:
+        return self.horas_trabajadas * self.tarifa_hora
