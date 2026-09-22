963c09b9042a6b8ab3e74fa742040c4e4e96cf92
Sieguart <sieguart1493@gmail.com>
2026-09-22 10:11:17 -0500

    Se agrego planilla

--- Trabajo_S2/Caso_Hotel_v2.py
+++ Trabajo_S2/Caso_Hotel_v2.py
@@ -68,3 +68,28 @@ class Empleado(ABC):
     def calcular_sueldo_neto(self) -> float:
         """Método abstracto polimórfico a implementar en subclases"""
         pass
+
+
+# ==========================================
+# 3. SUBCLASES CONCRETAS
+# ==========================================
+class EmpleadoPlanilla(Empleado):
+    def __init__(self, dni: str, nombre: str, sueldo_base: float, asignacion_familiar: float = 102.50):
+        super().__init__(dni, nombre, sueldo_base)
+        self._asignacion_familiar = asignacion_familiar
+
+    def calcular_sueldo_neto(self) -> float:
+        descuentos = self.calcular_afp() + self.calcular_impuesto_renta()
+        ingresos = self._sueldo_base + self._asignacion_familiar
+        return ingresos - descuentos
+
+
+class EmpleadoPorHoras(Empleado):
+    def __init__(self, dni: str, nombre: str, horas_trabajadas: int, tarifa_hora: float):
+        super().__init__(dni, nombre, horas_trabajadas * tarifa_hora)
+        self.horas_trabajadas = horas_trabajadas
+        self.tarifa_hora = tarifa_hora
+
+    def calcular_sueldo_neto(self) -> float:
+        descuentos = self.calcular_afp()
+        return (self.horas_trabajadas * self.tarifa_hora) - descuentos
